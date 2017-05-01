a3edbb6e3a2bd12a96b9b4b8314ffa4a986ce355
Woojin Joe <jwj0831@gmail.com>
2017-05-01 10:49:51 +0900

    3장-[ng-welcome-msg-app] 컴포넌트 생명주기 활용

--- ch03/ng-welcome-msg-app/src/app/app.component.ts
+++ ch03/ng-welcome-msg-app/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   userName = '';
+  private valid = false;
+  private static CHK_KEYUP_WAIT_SEC = 5000;
+
+  ngAfterViewInit() {
+    const checkTouchedFn = () => {
+      if (this.valid) return;
+      alert('이름을 입력해 주세요');
+    };
+
+    setTimeout(checkTouchedFn, AppComponent.CHK_KEYUP_WAIT_SEC);
+  }
+
+  onKeyUp(name) {
+    this.valid = name.length > 0;
+  }
 
   setName(name) {
     this.userName = name;
