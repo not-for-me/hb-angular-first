9b7bbb86b3de301eab3392d7c16b10530419325c
Woojin Joe <jwj0831@gmail.com>
2017-05-01 14:39:47 +0900

    5장-[mouse-pos-logger] MySpecialLoggerService 구현

--- ch05/mouse-pos-logger/src/app/my-special-logger.service.ts
+++ ch05/mouse-pos-logger/src/app/my-special-logger.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { LogLevel } from './log-level.enum';
+import * as format from 'date-fns/format';
 
 @Injectable()
 export class MySpecialLoggerService {
@@ -8,7 +10,39 @@ export class MySpecialLoggerService {
   private readonly TIME_FORMATTER: string = "YYYY-MM-DD HH:mm:ss.SSS";
 
   constructor(logLevel: LogLevel) {
-    this.logLevel = logLevel;
+    this.logLevel = logLevel
+  };
+
+
+  debug(msg: string) { this.log(LogLevel.DEBUG, msg); }
+  info(msg: string) { this.log(LogLevel.INFO, msg); }
+  warn(msg: string) { this.log(LogLevel.WARN, msg); }
+  error(msg: string) { this.log(LogLevel.ERROR, msg); }
+
+
+  log(logLevel: LogLevel, msg: string) {
+    const logMsg = this.getFormattedLogMsg(logLevel, msg);
+    if (this.isProperLogLevel(logLevel)) {
+      console.log(logMsg);
+      this.keepLogHistory(logMsg);
+    }
+  }
+
+  private keepLogHistory(log: string) {
+    if (this.logs.length === this.MAX_HISTORY_CNT) {
+      this.logs.shift();
+    }
+    this.logs.push(log);
+  }
+
+  private getFormattedLogMsg(logLevel: LogLevel, msg: string) {
+    const curTimestamp = format(new Date(), this.TIME_FORMATTER);
+    return `[${LogLevel[logLevel]}] ${curTimestamp} - ${msg}`;
+  }
+
+  private isProperLogLevel(logLevel: LogLevel): boolean {
+    if (this.logLevel === LogLevel.DEBUG) return true;
+    return logLevel >= this.logLevel;
   }
 
 }
