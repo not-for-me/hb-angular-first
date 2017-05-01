185d3ccdafe6889662dc69c180678a6c98975f5f
Woojin Joe <jwj0831@gmail.com>
2017-05-01 12:39:19 +0900

    3장-[ng-welcome-msg-app] 한글 바인딩 설정 추가

--- ch03/ng-welcome-msg-app/src/app/app.module.ts
+++ ch03/ng-welcome-msg-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, COMPOSITION_BUFFER_MODE } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -18,7 +18,7 @@ import { LangSelectorComponent } from './lang-selector/lang-selector.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [{provide: COMPOSITION_BUFFER_MODE, useValue: false}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
