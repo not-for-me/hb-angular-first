7d9c249e804248cdf6a71b006ad87e7b615e8b3e
Woojin Joe <jwj0831@gmail.com>
2017-05-01 10:43:50 +0900

    3장-[ng-welcome-msg-app] 앵귤러 방식으로 개선

--- ch03/ng-welcome-msg-app/src/app/app.component.ts
+++ ch03/ng-welcome-msg-app/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  userName = '';
+
+  setName(name) {
+    this.userName = name;
+  }
 }
