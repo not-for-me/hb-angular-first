43bf9d65fd9e85e2b3496858ddd3717b6438c8c1
Woojin Joe <jwj0831@gmail.com>
2017-05-01 10:53:52 +0900

    3장-[ng-welcome-msg-app] 소스 이동

--- ch03/ng-welcome-msg-app/src/app/app.component.ts
+++ ch03/ng-welcome-msg-app/src/app/app.component.ts
@@ -1,29 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  userName = '';
-  private valid = false;
-  private static CHK_KEYUP_WAIT_SEC = 5000;
-
-  ngAfterViewInit() {
-    const checkTouchedFn = () => {
-      if (this.valid) return;
-      alert('이름을 입력해 주세요');
-    };
-
-    setTimeout(checkTouchedFn, AppComponent.CHK_KEYUP_WAIT_SEC);
-  }
-
-  onKeyUp(name) {
-    this.valid = name.length > 0;
-  }
-
-  setName(name) {
-    this.userName = name;
-  }
-}
+export class AppComponent  { }
--- ch03/ng-welcome-msg-app/src/app/welcome-msg/welcome-msg.component.ts
+++ ch03/ng-welcome-msg-app/src/app/welcome-msg/welcome-msg.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-msg',
   templateUrl: './welcome-msg.component.html',
   styleUrls: ['./welcome-msg.component.css']
 })
-export class WelcomeMsgComponent implements OnInit {
+export class WelcomeMsgComponent implements AfterViewInit {
+  userName = '';
+  private valid = false;
+  private static CHK_KEYUP_WAIT_SEC = 5000;
 
-  constructor() { }
+  ngAfterViewInit() {
+    const checkTouchedFn = () => {
+      if (this.valid) return;
+      alert('이름을 입력해 주세요');
+    };
 
-  ngOnInit() {
+    setTimeout(checkTouchedFn, WelcomeMsgComponent.CHK_KEYUP_WAIT_SEC);
   }
 
+  onKeyUp(name) {
+    this.valid = name.length > 0;
+  }
+
+  setName(name) {
+    this.userName = name;
+  }
 }
