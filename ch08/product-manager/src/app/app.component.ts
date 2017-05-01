0c322e85cadc05a78abf880f38982a4eb4ff29a9
Woojin Joe <jwj0831@gmail.com>
2017-05-02 06:55:50 +0900

    8장-[product-manager] 기본 예제 코드 구현

--- ch08/product-manager/src/app/app.component.ts
+++ ch08/product-manager/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pm works!';
+  title = '상품 등록';
+  product: any;
+
+  constructor() {
+    this.initProduct();
+  }
+
+  initProduct() {
+    this.product = { name: '', listPrice: '', qty: 0, desc: '' };
+  }
+
+  onSubmit() {
+    alert(`제출\n${JSON.stringify(this.product)}`);
+  }
+
+  onReset() {
+    this.initProduct();
+  }
 }
